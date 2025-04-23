56993b6dc99a061fe424adcfad4f8d8d7db08bc5
d1temnd <74726976+d1temnd@users.noreply.github.com>
2025-04-23 22:18:48 +0700

    Enhance User model: enforce unique name constraint and add password hashing methods

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,12 +1,18 @@
 from models import db
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String(255), nullable=False, unique=True)
     rule = db.Column(db.String(50), nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
 
     posts = db.relationship('Post', backref='author', lazy=True)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
