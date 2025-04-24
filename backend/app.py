022c961912c35c373d74e2816a53ed6cb21469f6
d1temnd <74726976+d1temnd@users.noreply.github.com>
2025-04-24 23:51:05 +0700

    Refactor app initialization: move Flask app configuration to config.py and update app context usage in app.py

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from config import app_conf
 from models import db
 from models.user import User  # Явно импортируем модели
 from models.channel import Channel
@@ -6,19 +6,14 @@ from models.post import Post
 from models.media import Media
 from api import register_routes
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://myuser:mypassword@localhost:5432/smm'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SESSION_TYPE'] = 'filesystem'
-app.config['SECRET_KEY'] = 'your-secret-key'  # Секретный ключ для подписи сессий
 
-db.init_app(app)
-register_routes(app)
+db.init_app(app_conf.app)
+register_routes(app_conf.app)
 
 if __name__ == '__main__':
-    with app.app_context():
+    with app_conf.app.app_context():
         
         db.create_all()
         print("Таблицы созданы:", list(db.metadata.tables.keys()))
     
-    app.run(debug=True, host="0.0.0.0")
+    app_conf.app.run(debug=True, host="0.0.0.0")
--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,15 @@
 import boto3
 from botocore.client import Config
+from flask import Flask
+
+
+class app_conf:
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://myuser:mypassword@localhost:5432/smm'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SESSION_TYPE'] = 'filesystem'
+    app.config['SECRET_KEY'] = 'your-secret-key'  # Секретный ключ для подписи сессий
+
 
 class boto3_conf:
     S3_BUCKET_NAME = 'bucket-de6ad0'
